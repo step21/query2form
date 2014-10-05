771644967c09a5a4459d760fc7189d98e5c15f25
Jon Phillips <jon@fabricatorz.com>
2014-10-05 04:15:39 +0000

    addedfirst

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/* This is query2form. It takes a query string, converts to usable form, 
+   emailing you the results.
+
+
+    Input: //localhost/query2form/jon@rejon.org
+
+    Input: //localhost/query2form?email=jon@rejon.org&title=Some%20Title
+
+    Output: Bootstrap form
+
+
+*/
+
+?>
